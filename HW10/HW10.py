9cd592f4d4035772fd2682e688c5ba1d1cce55e1
user <user@cu-cs-vm.(none)>
2014-05-01 10:38:41 -0600

    Blackbody Graphing Multiple with Lables

---
+++ HW10/HW10.py
@@ -0,0 +1,10 @@
+#File: HW10.py
+#Name: Chris Fichman
+#Email: chris.fichman@gmail.com
+
+from numpy import *
+from matplotlib import *
+from pylab import *
+from math import *
+
+angstroms, counts = loadtxt('correl.txt', delimiter = ',', unpack=True)
